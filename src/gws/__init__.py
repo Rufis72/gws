a597852a5c254ab4b956ca04bea842415616f382
Rufis72 <milo.games.silverstein@gmail.com>
2026-06-17 21:32:58 -0700

    Added __init__.pys

---
+++ src/gws/__init__.py
@@ -0,0 +1,7 @@
+import gws.window as window
+import gws.window_managers as window_managers
+
+__all__ = [
+    'window',
+    'window_managers'
+]
---
+++ src/gws/window_managers/__init__.py
@@ -0,0 +1,5 @@
+from gws.window_managers import hyprland
+
+__all__ = [
+    'hyprland'
+]
