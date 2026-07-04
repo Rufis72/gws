add3c681d7d6cdde0d8674ebd78d7d8b1e67234a
Rufis72 <milo.games.silverstein@gmail.com>
2026-07-03 21:13:16 -0700

    Added a way to check if the needed external dependencies are installed, updated the top level __init__

--- src/gws/__init__.py
+++ src/gws/__init__.py
@@ -1,9 +1,17 @@
 import gws.window as window
 import gws.window_managers as window_managers
 import gws.application_implementations as application_implementations
+import gws._errors as _errors
+import gws._generics as _generics
+import gws._typing as _typing
+import gws._dependency_management as _dependency_management
 
 __all__ = [
     'window',
     'window_managers',
     'application_implementations',
+    '_errors',
+    '_generics',
+    '_typing',
+    '_dependency_management',
 ]
---
+++ src/gws/_dependency_management.py
@@ -0,0 +1,45 @@
+'''For future reference, this file is intended to be
+used to make sure every external dependency needed
+can be checked for to make sure it's installed'''
+import shutil
+from functools import wraps
+
+# checking every dependency we need to see if it's installed
+wtype_installed: bool = shutil.which('wtype') is not None
+wayland_utils_installed: bool = shutil.which('wayland-info') is not None
+grim_installed = shutil.which('grim') is not None
+
+
+# making a bunch of decorators for each dependency
+def requires_wtype(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # if wtype isn't installed, raising an error
+        if not wtype_installed:
+            raise RuntimeError(f'wtype isn\'t installed, and is required to do typing input on Wayland. Are you sure you\'ve installed it/it\'s on path as "wtype"?')
+        
+        # running the function we're decorating
+        return func(*args, **kwargs)
+    return wrapper
+
+def requires_wayland_utils(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # if wayland_utils isn't installed, raising an error
+        if not wayland_utils_installed:
+            raise RuntimeError(f'wayland_utils isn\'t installed, and is required to get monitor info on Wayland. Are you sure you\'ve installed it/it\'s on path as "wayland-info"?')
+        
+        # running the function we're decorating
+        return func(*args, **kwargs)
+    return wrapper
+
+def requires_grim(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # if grim isn't installed, raising an error
+        if not grim_installed:
+            raise RuntimeError(f'grim isn\'t installed, and is required to take screenshots on Wayland. Are you sure you\'ve installed it/it\'s on path as "grim"?')
+        
+        # running the function we're decorating
+        return func(*args, **kwargs)
+    return wrapper
