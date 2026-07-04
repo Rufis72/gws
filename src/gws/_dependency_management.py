a0678cbcad030d5577022d5931eabf79f24d073a
Rufis72 <milo.games.silverstein@gmail.com>
2026-07-03 21:21:58 -0700

    Fixed grammar, clariedied that wayland-utils being on path as wayland-info isn't a mistake

--- src/gws/_dependency_management.py
+++ src/gws/_dependency_management.py
@@ -16,7 +16,7 @@ def requires_wtype(func):
     def wrapper(*args, **kwargs):
         # if wtype isn't installed, raising an error
         if not wtype_installed:
-            raise RuntimeError(f'wtype isn\'t installed, and is required to do typing input on Wayland. Are you sure you\'ve installed it/it\'s on path as "wtype"?')
+            raise RuntimeError(f'wtype isn\'t installed, and is required to do typing input on Wayland. Are you sure you\'ve installed/it\'s on path as "wtype"?')
         
         # running the function we're decorating
         return func(*args, **kwargs)
@@ -27,7 +27,7 @@ def requires_wayland_utils(func):
     def wrapper(*args, **kwargs):
         # if wayland_utils isn't installed, raising an error
         if not wayland_utils_installed:
-            raise RuntimeError(f'wayland_utils isn\'t installed, and is required to get monitor info on Wayland. Are you sure you\'ve installed it/it\'s on path as "wayland-info"?')
+            raise RuntimeError(f'wayland_utils isn\'t installed, and is required to get monitor info on Wayland. Are you sure you\'ve installed/it\'s on path as "wayland-info"? \n\n(Note: the package being wayland-utils, but it\'s on path as wayland-info isn\'t a mistake, it\'s what the package chose.)')
         
         # running the function we're decorating
         return func(*args, **kwargs)
@@ -38,7 +38,7 @@ def requires_grim(func):
     def wrapper(*args, **kwargs):
         # if grim isn't installed, raising an error
         if not grim_installed:
-            raise RuntimeError(f'grim isn\'t installed, and is required to take screenshots on Wayland. Are you sure you\'ve installed it/it\'s on path as "grim"?')
+            raise RuntimeError(f'grim isn\'t installed, and is required to take screenshots on Wayland. Are you sure you\'ve installed/it\'s on path as "grim"?')
         
         # running the function we're decorating
         return func(*args, **kwargs)
